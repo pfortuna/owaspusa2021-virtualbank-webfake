489855b6ab84055d47b16b068052526b2e7f6a20
Jasvir Nagra <jasvir@gmail.com>
2021-11-10 15:06:17 -0800

    Increase the screen size

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
+    args: [`--window-size=1920,1080`]
   });
   const page = await browser.newPage();
   await page.goto('http://virtualbank.com:5000/', {waitUntil: 'load'});
